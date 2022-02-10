bdc8c6725b37aa359b59c307f824cf0b11ab353a
Yanislav Igonin <yanislavdabest@gmail.com>
2022-02-10 19:39:34 +0200

    feat: add boards endpoint

--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	Config "micrach-gateway/config"
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/joho/godotenv/autoload"
 )
+
+type Board struct {
+	ID          string `json:"id"`
+	Description string `json:"description"`
+}
 
 func main() {
 	app := fiber.New()
+	Config.Init()
 
-	app.Get("/", func(c *fiber.Ctx) error {
+	boards := make(map[string]Board)
+
+	app.Post("/boards", func(c *fiber.Ctx) error {
+		headerApiKey := c.Get("Authorization")
+		if headerApiKey != Config.App.ApiKey {
+			return c.Status(401).JSON(fiber.Map{
+				"error": "Unauthorized",
+			})
+		}
+		board := Board{}
+		c.BodyParser(&board)
+		boards[board.ID] = board
 		return c.SendString("Hello, World 👋!")
 	})
 
-	app.Listen(":3000")
+	app.Listen(":" + strconv.Itoa(Config.App.Port))
 }
