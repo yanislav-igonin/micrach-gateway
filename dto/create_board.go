51845b41bd66feae8ec4387a1efe8fe1e2c4cecb
Yanislav Igonin <yanislavdabest@gmail.com>
2022-02-21 14:35:59 +0200

    update create board dto

--- dto/create_board.go
+++ dto/create_board.go
@@ -1,7 +1,7 @@
 package dto
 
 type CreateBoardRequest struct {
-	ID          string `json:"id"`
-	Description string `json:"description"`
-	Url         string `json:"url"`
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Url  string `json:"url"`
 }
