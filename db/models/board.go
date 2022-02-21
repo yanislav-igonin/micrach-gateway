d22ef093e2fb0da0d5db72d410aaebb61aca992a
Yanislav Igonin <yanislavdabest@gmail.com>
2022-02-21 14:35:31 +0200

    update board model

--- db/models/board.go
+++ db/models/board.go
@@ -1,14 +1,20 @@
 package models
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Board struct {
-	_ID         primitive.ObjectID `bson:"_id"`
-	ID          string             `bson:"id"`
-	Description string             `bson:"description"`
-	Url         string             `bson:"url"`
-	LastPing    int64              `bson:"lastPing"`
-	IsUp        bool               `bson:"isUp"`
+	ID        primitive.ObjectID `bson:"_id"`
+	Shortcut  string             `bson:"shortcut"`
+	Name      string             `bson:"name"`
+	Url       string             `bson:"url"`
+	CreatedAt time.Time          `bson:"createdAt"`
+	UpdatedAt time.Time          `bson:"updatedAt"`
+	// for the next update
+	// LastPing       time.Time              `bson:"lastPing"`
+	// IsUp           bool               `bson:"isUp"`
+	// PingRetryCount int                `bson:"pingRetryCount"`
 }
