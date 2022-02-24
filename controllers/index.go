97bc330740f2ed353ef5842d5123707986c6cba6
Yanislav Igonin <yanislavdabest@gmail.com>
2022-02-24 12:26:45 +0200

    finally li rendered

--- controllers/index.go
+++ controllers/index.go
@@ -1,9 +1,20 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+	Repositories "micrach-gateway/db/repositories"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Index(ctx *fiber.Ctx) error {
+	boards, err := Repositories.Boards.GetAll()
+	log.Println(boards)
+	if err != nil {
+		return fiber.ErrInternalServerError
+	}
 	return ctx.Render("index", fiber.Map{
-		"Title": "Hello, World!",
+		"Title":  "Hello, World!",
+		"Boards": boards,
 	}, "layout/main")
 }
